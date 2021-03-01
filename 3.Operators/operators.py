e163010f118b2bca843a32942372ac9c1936fc60
bharathims <65299754+bharathims@users.noreply.github.com>
2021-03-01 09:43:12 +0000

    Add files via upload

---
+++ 3.Operators/operators.py
@@ -0,0 +1,41 @@
+#!/bin/python
+
+'''
+Basic python operators
+Author: Bharathi M
+Date: 2021-03-01
+'''
+
+a = 10
+b = 5
+
+print('A is ', a, 'B is ', b)
+print('a + b ', a + b)
+print('a - b ', a - b)
+print('a * b ', a * b)
+print('a / b ', a / b)
+print('a % b ', a % b)
+print('a ** b ', a ** b)
+print('a // b ', a // b)
+
+print('a == b ', a == b)
+print('a != b ', a != b)
+print('a > b ', a > b)
+print('a < b ', a < b)
+print('a >= b ', a <= b)
+
+string = 'Welcome'
+print(3 * '\n')
+print('Welcome' is string)
+print('Welcome' is not string)
+print('Welcome' in string)
+print('Welcome' not in string)
+
+a = True
+b = False
+
+print(3 * '\n')
+print('Welcome' is string)
+print('T and F -- ', a and b)
+print('T or F -- ', a or b)
+print('not T -- ', not a)
